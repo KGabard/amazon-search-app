eb9a383fea28ad1794e54f3f27ab6ace804fcb7e
KGabard <kev.gabard@gmail.com>
2023-07-12 12:57:40 +0200

    Ajout d'un bouton retour sur la page produit

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -5,13 +5,19 @@ import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import EastRoundedIcon from '@mui/icons-material/EastRounded'
 
 import { usePathname } from 'next/navigation'
 
 import { useTheme } from '@mui/material/styles'
+import Link from 'next/link'
 
 export default function Header() {
   const theme = useTheme()
+
+  const pathname = usePathname()
+
+  const isHomePage = pathname === '/'
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -26,6 +32,36 @@ export default function Header() {
           <Typography component="h1" variant="title" sx={{ flexGrow: 1 }}>
             Amazon Search
           </Typography>
+          {!isHomePage && (
+            <Box
+              sx={{
+                display: 'flex',
+                gap: '6px',
+                alignItems: 'center',
+              }}
+            >
+              <Link
+                href={`/`}
+                style={{
+                  display: 'inline-block',
+                  width: 'fit-content',
+                }}
+              >
+                <Typography
+                  component={'span'}
+                  variant="subtitle"
+                  sx={{
+                    textDecoration: 'underline',
+                    textDecorationThickness: '2px',
+                    textUnderlineOffset: '4px',
+                  }}
+                >
+                  Back to Search
+                </Typography>
+              </Link>
+              <EastRoundedIcon />
+            </Box>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
