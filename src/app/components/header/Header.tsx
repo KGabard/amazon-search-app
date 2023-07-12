10c08cd0a905f3d704c1d1cb78aec329f03f1573
KGabard <kev.gabard@gmail.com>
2023-07-12 21:34:44 +0200

    Responsive du Header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -21,46 +21,54 @@ export default function Header() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        sx={{
+          height: 'fit-content',
+        }}
+      >
         <Toolbar
           sx={{
             backgroundColor: theme.palette.primary.main,
             color: theme.palette.white.main,
-            height: 'var(--header-height)',
+            display: 'flex',
+            flexDirection: 'column',
+            height: 'fit-content',
+            pt: '16px',
+            pb: '24px',
+
+            [theme.breakpoints.up('sm')]: {
+              flexDirection: 'row',
+              pb: '16px',
+            },
           }}
         >
           <Typography component="h1" variant="title" sx={{ flexGrow: 1 }}>
             Amazon Search
           </Typography>
           {!isHomePage && (
-            <Box
-              sx={{
+            <Link
+              href={`/`}
+              style={{
                 display: 'flex',
+                width: 'fit-content',
                 gap: '6px',
                 alignItems: 'center',
               }}
             >
-              <Link
-                href={`/`}
-                style={{
-                  display: 'inline-block',
-                  width: 'fit-content',
+              <Typography
+                component={'span'}
+                variant="subtitle"
+                sx={{
+                  textDecoration: 'underline',
+                  textDecorationThickness: '2px',
+                  textUnderlineOffset: '4px',
                 }}
               >
-                <Typography
-                  component={'span'}
-                  variant="subtitle"
-                  sx={{
-                    textDecoration: 'underline',
-                    textDecorationThickness: '2px',
-                    textUnderlineOffset: '4px',
-                  }}
-                >
-                  Back to Search
-                </Typography>
-              </Link>
+                Back to Search
+              </Typography>
               <EastRoundedIcon />
-            </Box>
+            </Link>
           )}
         </Toolbar>
       </AppBar>
